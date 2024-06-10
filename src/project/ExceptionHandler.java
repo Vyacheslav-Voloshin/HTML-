6b17ff37e70edb5ba864b170d25ab7ee174ff2f3
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-10 21:44:36 +0300

    Create class ExceptionHandler

---
+++ src/project/ExceptionHandler.java
@@ -0,0 +1,9 @@
+package project;
+
+
+//Клас обробник виняткових ситуацій
+public class ExceptionHandler {
+    public static void log(Exception e){
+        System.out.println(e.toString());
+    }
+}
