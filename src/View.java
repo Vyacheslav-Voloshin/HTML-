6192a9b205fbc9963b2a00227dc7c524c5e0fdbf
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-10 20:21:51 +0300

    Create class View and Controller. Add methods init() and exit() to them

---
+++ src/View.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void init(){}
+
+    public void exit(){
+        controller.exit();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
