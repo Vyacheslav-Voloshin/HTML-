aab01d9022e098f0c65df806f8697b253e2af8b4
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-12 20:49:29 +0300

    Implemented the method menuSelected of class UndoMenuListener

--- src/project/listeners/UndoMenuListener.java
+++ src/project/listeners/UndoMenuListener.java
@@ -5,11 +5,14 @@ import project.View;
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
-
+/*Наш редактор підтримуватиме механізм скасування/повернення (undo/redo) дій у редакторі.
+ Клас UndoMenuListener. Цей слухач стежитиме за меню, а якщо конкретніше, то за моментом,
+ коли меню редагування буде обрано користувачем. У цей момент він буде запитувати у подання чи можемо ми зараз скасувати або повернути якусь дію,
+ і в залежності від цього робити доступними або недоступними пункти меню "Скасувати" та "Повернути".*/
 public class UndoMenuListener implements MenuListener {
     private View view;
-    private JMenuItem undoMenuItem;
-    private JMenuItem redoMenuItem;
+    private JMenuItem undoMenuItem; //Пункт меню "Відмінити"
+    private JMenuItem redoMenuItem; //Пункт меню "Повернути"
 
     public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
         this.view = view;
@@ -18,8 +21,17 @@ public class UndoMenuListener implements MenuListener {
     }
 
     @Override
-    public void menuSelected(MenuEvent e) {
-
+    public void menuSelected(MenuEvent menuEvent) {
+        if (view.canUndo()){
+            undoMenuItem.setEnabled(true);
+        } else {
+            undoMenuItem.setEnabled(false);
+        }
+        if (view.canRedo()){
+            redoMenuItem.setEnabled(true);
+        } else {
+            redoMenuItem.setEnabled(false);
+        }
     }
 
     @Override
