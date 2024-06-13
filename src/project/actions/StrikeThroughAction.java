ac61f50b6d7e6510f9d681642a3a592352fd3359
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-13 21:46:48 +0300

    add code for all classes in package actions

--- src/project/actions/StrikeThroughAction.java
+++ src/project/actions/StrikeThroughAction.java
@@ -1,18 +1,26 @@
 package project.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 //Клас, який відповідає за стиль тексту "Зачеркнутый".
-public class StrikeThroughAction extends StyledEditorKit.StyledTextAction{
-
+public class StrikeThroughAction extends StyledEditorKit.StyledTextAction {
 
     public StrikeThroughAction() {
-        super("Зачеркнутый");
-
+        super(StyleConstants.StrikeThrough.toString());
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+//Таким чином, при виконанні даного коду, якщо виділений текст вже закреслено, то закреслення буде видалено, а якщо текст не було закреслено,
+// то закреслення буде додано
+    public void actionPerformed(ActionEvent actionEvent) {
+        JEditorPane editor = getEditor(actionEvent);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setStrikeThrough(simpleAttributeSet, !StyleConstants.isStrikeThrough(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
