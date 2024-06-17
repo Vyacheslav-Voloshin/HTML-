c1b433cd802f780602b393b82447853524b1675a
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-17 19:17:03 +0300

    add getter for field document

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -19,6 +19,10 @@ public class Controller {
         System.exit(0);
     }
 
+    public HTMLDocument getDocument() {
+        return document;
+    }
+
     public static void main(String[] args) {
       View view = new View();
       Controller controller = new Controller(view);
