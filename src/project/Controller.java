fcea7c66bc4b11e097f428db90d9bc48103afcd1
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-06-18 20:52:51 +0300

    create new method saveDocumentAs() in class Controller

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -1,11 +1,11 @@
 package project;
 
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.HTMLWriter;
-import java.io.File;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Controller {
 
@@ -69,8 +69,26 @@ public class Controller {
         currentFile = null;
     }
     public void openDocument(){}
+
     public void saveDocument(){}
-    public void saveDocumentAs(){}
+
+    //метод для збереження файлу під новим ім'ям
+    public void saveDocumentAs(){
+        view.selectHtmlTab();//Вибирати html вкладку у вистави.
+        JFileChooser jFileChooser = new JFileChooser(); //створюємо новий об'єкт для вибору файлу JFileChooser
+        jFileChooser.setFileFilter(new HTMLFileFilter()); //встановлювати об'єкту класу JFileChooser як фільтр об'єкт HTMLFileFilter
+        if (jFileChooser.showSaveDialog(view) == JFileChooser.APPROVE_OPTION){ //Якщо користувач підтвердить вибір файлу:
+            currentFile = jFileChooser.getSelectedFile(); //Зберігаємо вибраний файл у полі currentFile.
+            view.setTitle(currentFile.getName()); //Встановлюваємо ім'я файлу як заголовок вікна представлення.
+
+            try (FileWriter fileWriter = new FileWriter(currentFile)){ //Створюємо FileWriter з урахуванням currentFile.
+                HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+                htmlEditorKit.write(fileWriter,document,0,document.getLength()); //Переписуємо дані з документа в об'єкт FileWriter-а аналогічно тому, як ми це робили в методі getPlainText().
+            } catch (IOException | BadLocationException e) {
+                ExceptionHandler.log(e);
+            }
+        }
+    }
 
     public static void main(String[] args) {
       View view = new View();
